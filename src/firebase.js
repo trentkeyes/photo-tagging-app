36e690fe97af805c83cdf4444e2034d1f5df7049
Trent Keyes <tkeyes@gmail.com>
2023-01-05 16:48:58 -0600

    Change api key and add gitignore file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,19 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDC0bkVaG1Sa2BkwWUU_GcsqNerj6B4_Tk',
-  authDomain: 'photo-tagging-app-7bda4.firebaseapp.com',
-  projectId: 'photo-tagging-app-7bda4',
-  storageBucket: 'photo-tagging-app-7bda4.appspot.com',
-  messagingSenderId: '877683745112',
-  appId: '1:877683745112:web:9ad3d668471ad8b6c4452d',
-  measurementId: 'G-8MZ3173CFT',
+  apiKey: 'AIzaSyD3zXHAIf5YiR3vLVXDPXIueCTMlYPqJdM',
+
+  authDomain: 'photo-tagging-app-60af0.firebaseapp.com',
+
+  projectId: 'photo-tagging-app-60af0',
+
+  storageBucket: 'photo-tagging-app-60af0.appspot.com',
+
+  messagingSenderId: '325996091590',
+
+  appId: '1:325996091590:web:4c9533c4fdf38599bf9fc7',
+
+  measurementId: 'G-8E1KRYSXED',
 };
 
 const app = initializeApp(firebaseConfig);
